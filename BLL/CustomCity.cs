f06eafd34d3705cc73ba99c8f25f82b24e964830
geeko66 <karim.azzab@heig-vd.ch>
2015-06-06 19:36:06 +0200

    Digitard base project

---
+++ BLL/CustomCity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Digitard.DAL
+
+namespace BLL
+{
+    public partial class City
+    {
+      private readonly DigitardEntities1 ctx = new DigitardEntities1();
+
+      public City(String name)
+      {
+        this.
+      }
+
+      public bool Add()
+      {
+
+      }
+      public List<City> GetCities()
+      {
+
+      }
+    }
+}
